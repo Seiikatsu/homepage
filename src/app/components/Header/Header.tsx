b4f712fc66368f0e8283ed7d7f8abce45e10b28e
Seikatsu <seikatsu@seikatsu.io>
2021-08-11 21:38:53 +0200

    - add grid component to header

--- src/app/components/Header/Header.tsx
+++ src/app/components/Header/Header.tsx
@@ -2,26 +2,34 @@ import styled, {css} from "styled-components";
 import {useState} from "react";
 import {Span} from "../Text";
 import {Li, Ul} from "../List";
-import {commonMaxWidthMedia} from "../../../styles/common";
+import {Col, Container, Row} from "../Grid";
 import content from './Logo.svg';
 
 export function Header() {
 	const [fixed, setFixed] = useState<boolean>(false);
 	return (
 		<HeaderComponent fixed={fixed}>
-			<Wrapper>
-				<img src={content} height={48} width={48}/>
-				<div style={{display: 'none'}}>Mobile nav opener</div>
-				<nav>
-					<Ul>
-						<Li><Span active>Home</Span></Li>
-						<Li><Span primary>About</Span></Li>
-						<Li><Span primary>Services</Span></Li>
-						<Li><Span primary>Portfolio</Span></Li>
-						<Li><Span primary>Contact</Span></Li>
-					</Ul>
-				</nav>
-			</Wrapper>
+			<Container>
+				<Row>
+					<Col columnSize={1}>
+						<img src={content} height={48} width={48}/>
+					</Col>
+					<Col columnSize={0}>
+						<div style={{display: 'none'}}>Mobile nav opener</div>
+					</Col>
+					<Col columnSize={4}>
+						<Nav>
+							<Ul>
+								<Li><Span active>Home</Span></Li>
+								<Li><Span primary>About</Span></Li>
+								<Li><Span primary>Services</Span></Li>
+								<Li><Span primary>Portfolio</Span></Li>
+								<Li><Span primary>Contact</Span></Li>
+							</Ul>
+						</Nav>
+					</Col>
+				</Row>
+			</Container>
 		</HeaderComponent>
 	);
 }
@@ -49,17 +57,7 @@ const HeaderComponent = styled.header<HeaderProps>`
   `}
 `;
 
-const Wrapper = styled.div`
-  width: 100%;
-
+const Nav = styled.nav`
   display: flex;
-  flex-direction: row;
-  align-items: center;
-  justify-content: space-between;
-  //flex-wrap: nowrap;
-
-  padding-inline: 1em;
-  margin-inline: auto;
-
-  ${commonMaxWidthMedia};
+  justify-content: right;
 `;
