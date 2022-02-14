4da22115063888384a96058165c54b013bd5cbcc
Seikatsu <seikatsu@seikatsu.io>
2022-02-14 20:07:52 +0100

    remove minHeight property from section as it isn't used any more (landing has calculated height)

--- src/app/pages/IndexPage/components/Section.ts
+++ src/app/pages/IndexPage/components/Section.ts
@@ -1,44 +1,39 @@
-import styled, { css } from "styled-components";
+import styled, {css} from "styled-components";
 
 export type SectionProps = {
-  backgroundImage?: string;
-  opacity?: number;
-  minHeight?: string;
+	backgroundImage?: string;
+	opacity?: number;
 };
 
 export const Section = styled.section<SectionProps>`
   padding-block: 100px;
 
-  ${(p) => p.minHeight && css`
-    min-height: ${p.minHeight};
-  `}
-  
   ${(p) =>
-    !p.backgroundImage &&
-    css`
-      background-color: ${(p) => p.theme.backgroundVariant};
-    `}
+		  !p.backgroundImage &&
+		  css`
+			background-color: ${(p) => p.theme.backgroundVariant};
+		  `}
 
   ${(p) =>
-    p.backgroundImage &&
-    css`
-      background-image: url("${p.backgroundImage}");
-      background-attachment: fixed;
-      background-size: cover;
-      background-repeat: no-repeat;
-      background-position: center;
-      position: relative;
+		  p.backgroundImage &&
+		  css`
+			background-image: url("${p.backgroundImage}");
+			background-attachment: fixed;
+			background-size: cover;
+			background-repeat: no-repeat;
+			background-position: center;
+			position: relative;
 
-      &::before {
-        content: "";
-        position: absolute;
-        width: 100%;
-        height: 100%;
-        top: 0;
-        left: 0;
-        z-index: 1;
-        background-color: #000;
-        opacity: 0.8;
-      }
-    `}
+			&::before {
+			  content: "";
+			  position: absolute;
+			  width: 100%;
+			  height: 100%;
+			  top: 0;
+			  left: 0;
+			  z-index: 1;
+			  background-color: #000;
+			  opacity: 0.8;
+			}
+		  `}
 `;
